e5571a2f5040d9d9d4918e33c4a827578480c429
Pedro Juan <pedro.juan29@hotmail.com>
2022-11-18 12:32:51 -0300

    first commit

---
+++ ProjetoFaculdade/Controllers/EventoController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjetoFaculdade.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoFaculdade.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]
+            {
+                new Evento()
+                {
+                EventoId = 1,
+                Tema = "Angular",
+                Local = "SP",
+                Lote = "1 lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "Foto 1"
+
+                },
+                new Evento()
+                {
+                EventoId = 2,
+                Tema = "Angular 2 teste",
+                Local = "SP",
+                Lote = "2 lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "Foto 2"
+
+                }
+            };
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetByid(int id)
+        {
+            return _evento.Where(x => x.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo put {id}";
+        }
+    }
+}
